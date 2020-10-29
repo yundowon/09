9e174fb032b657216618f6bed396b0f86eb7f027
yundowon <70631088+yundowon@users.noreply.github.com>
2020-10-29 19:14:05 +0900

    03

--- main.c
+++ main.c
@@ -6,20 +6,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum=0;
 	for(i=0;i<SIZE;i++)
-	{
-		printf("학생 성적을 입력하세요: ");
-		scanf("%d", &grade[i]);
-		sum += grade[i]; 
-	}
+		grade[i]=rand()%100+1;
 	
-	average=sum/SIZE;
-	printf("성적 평균: %d\n", average);
+	for(i=0;i<SIZE;i++)
+		score[i]=grade[i];
+	
+	for(i=0;i<SIZE;i++)
+		printf("score[%d] %i (%i)\n", i, score[i], grade[i]);
 		
 	return 0;
 }
